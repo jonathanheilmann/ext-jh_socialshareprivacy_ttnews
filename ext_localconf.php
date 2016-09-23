7bf1879acb64ebf89b349c8bd445f97d695d1bc0
jonathanheilmann <mail@jonathan-heilmann.de>
2016-09-23 21:47:09 +0200

    [TASK] Code clean up

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,5 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
+if (!defined ('TYPO3_MODE'))
      die ('Access denied.');
-}
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tt_news']['extraItemMarkerHook'][] = 'EXT:jh_socialshareprivacy_ttnews/Classes/Hooks/TxTtNews.php:\Heilmann\JhSocialshareprivacyTtnews\Hooks\TxTtNews';
-?>
